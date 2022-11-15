184e9aba3a3b306e1c007e979a7e0d834712f797
Joerg Heinrich <contact@jfheinrich.eu>
2022-11-15 23:28:25 +0100

    feat(#2): Dependencies removed and Test updated to a coverage of 100%

--- tests/Console/Commands/DtoMakeCommandTest.php
+++ tests/Console/Commands/DtoMakeCommandTest.php
@@ -2,10 +2,12 @@
 
 declare(strict_types=1);
 
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Facades\File;
 use JfheinrichEu\LaravelMakeCommands\Console\Commands\DtoMakeCommand;
 
 use function PHPUnit\Framework\assertTrue;
+use Mockery\MockInterface;
 
 it('can run the command successfully', function () {
     $this
@@ -25,3 +27,22 @@ it('create the data transfer object when called', function (string $class) {
         ),
     );
 })->with('classes');
+
+it('check getStub() method', function() {
+    $test = new DtoMakeCommand(new Filesystem());
+
+    $reflection = new ReflectionClass(
+        objectOrClass: DtoMakeCommand::class,
+    );
+
+    $property = $reflection->getProperty('dir');
+    $dir = $property->getValue($test);
+
+    $method = $reflection->getMethod('getStub');
+    $method->setAccessible(true);
+
+    $expected = $dir . '/../../../stubs/dto.stub';
+    $stub = $method->invoke($test);
+
+    $this->assertEquals($expected, $stub);
+});
