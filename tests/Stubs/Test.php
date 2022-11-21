1f29eabf052617111790cd29038a64c03a86fa1d
Joerg Heinrich <contact@jfheinrich.eu>
2022-11-21 05:55:35 +0100

    fest(#4): The DTO cqn now return a Collection and use a trait

--- tests/Stubs/Test.php
+++ tests/Stubs/Test.php
@@ -5,21 +5,14 @@ declare(strict_types=1);
 namespace JfheinrichEu\LaravelMakeCommands\Tests\Stubs;
 
 use JfheinrichEu\LaravelMakeCommands\Contracts\DtoContract;
+use JfheinrichEu\LaravelMakeCommands\Traits\useDTO;
 
 final class Test implements DtoContract
 {
+    use useDTO;
+
     public function __construct(
         private readonly string $name,
     ) {
-    }
-
-    public function JsonSerialize(): mixed
-    {
-        return $this->toArray();
-    }
-
-    public function toArray(): array
-    {
-        return get_object_vars($this);
     }
 }
