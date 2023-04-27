833597c6edb94a6841d62adffee3c34e07010ccd
Joerg Heinrich <contact@jfheinrich.eu>
2023-04-27 21:43:25 +0200

    LMC-6: JfhDatabaseSeeder implemented

--- tests/PackageTestCase.php
+++ tests/PackageTestCase.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 namespace JfheinrichEu\LaravelMakeCommands\Tests;
 
+use Illuminate\Config\Repository;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use JfheinrichEu\LaravelMakeCommands\LaravelMakeCommandsPackageProvider;
 use Orchestra\Testbench\TestCase;
 
 class PackageTestCase extends TestCase
 {
+    use RefreshDatabase;
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -23,5 +28,28 @@ class PackageTestCase extends TestCase
         return [
             LaravelMakeCommandsPackageProvider::class,
         ];
+    }
+
+    protected function defineEnvironment(Application $app): void
+    {
+        tap($app->make('config'), function (Repository $config)
+        {
+            $config->set('database.default', 'testbench');
+            $config->set('database.connections.testbench', [ 
+                'driver'   => 'sqlite',
+                'database' => ':memory:',
+                'prefix'   => '',
+            ]);
+
+            $config([ 
+                'queue.batching.database' => 'testbench',
+                'queue.failed.database'   => 'testbench',
+            ]);
+        });
+    }
+
+    protected function defineDatabaseMigrations(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
     }
 }
