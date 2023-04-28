31127067d12a00d567722289ffe058956960cfd8
Joerg Heinrich <joerg@jfheinrich.eu>
2023-04-28 14:54:53 +0200

    LMC-6: Base JSON seeder implemented

--- src/Support/Database/Seeder/DatabaseJsonSeeder.php
+++ src/Support/Database/Seeder/DatabaseJsonSeeder.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace JfheinrichEu\LaravelMakeCommands\Support\Database\Seeder;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-class JfhDatabaseSeeder extends Seeder
+class DatabaseJsonSeeder extends Seeder
 {
     use WithoutModelEvents;
 
     public function run(): void
     {
-        $seeders = Config::get('make-commands.seeders', []);
+        /** @var array<int,class-string> $seeders */
+        $seeders = Config::get('make-commands.seeders.classes', []);
 
         if (is_array($seeders)) {
             $this->call($seeders);
