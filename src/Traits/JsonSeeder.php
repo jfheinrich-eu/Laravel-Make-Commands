312d28a020bfbdb9d6741d36a77612e9a1c53c11
Joerg Heinrich <contact@jfheinrich.eu>
2023-05-01 23:49:29 +0200

    LMC-2: PHPstan issues

--- src/Traits/JsonSeeder.php
+++ src/Traits/JsonSeeder.php
@@ -66,7 +66,7 @@ trait JsonSeeder
      * @throws ParseError
      * @return mixed
      */
-    protected function createSeederObject(string $model) : mixed
+    protected function createSeederObject(string $model): mixed
     {
         $seederName = class_basename($model) . 'Seeder';
         $seederFqn = 'JfheinrichEu\LaravelMakeCommands\Support\Database\Seeder\\' . $seederName;
--- tests/Units/Dto/RepositoryDtoTest.php
+++ tests/Units/Dto/RepositoryDtoTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace JfheinrichEu\LaravelMakeCommands\Tests\Units\Dto;
 
-use Illuminate\Support\Collection;
 use JfheinrichEu\LaravelMakeCommands\Dto\DataTransferObject;
 use JfheinrichEu\LaravelMakeCommands\Dto\RepositoryDto;
 use JfheinrichEu\LaravelMakeCommands\Tests\PackageTestCase;
